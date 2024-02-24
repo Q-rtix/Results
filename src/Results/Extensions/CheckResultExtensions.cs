30b83a3666433b961a61bf40bac6ccf81d3a63a5
Carlos J. Ortiz <ortizdcj@gmail.com>
2024-02-24 14:25:28 -0500

    Add CheckResultExtensions for result evaluations
    A new static class, CheckResultExtensions, is introduced in the "Results/Extensions" directory. This has been implemented to provide methods for evaluating the success or failure of result objects. It checks if the results comply with given conditions and returns boolean values appropriately.

---
+++ src/Results/Extensions/CheckResultExtensions.cs
@@ -0,0 +1,40 @@
+using Results.ResultTypes;
+
+namespace Results.Extensions;
+
+/// <summary>
+/// Provides extension methods for the Check Result operations.
+/// </summary>
+public static class CheckResultExtensions
+{
+	/// <summary>
+	/// Checks if the result is successful and satisfies the given condition.
+	/// </summary>
+	/// <typeparam name="TValue">The type of the value contained in case of successful result.</typeparam>
+	/// <param name="self">The result object to check.</param>
+	/// <param name="function">The condition function to apply on the result value.</param>
+	/// <returns>True if the result is successful and satisfies the condition, otherwise false.</returns>
+	public static bool IsSucceedAnd<TValue>(this Result<TValue> self, Func<TValue, bool> function)
+		=> self.IsSucceed && function(self.Value);
+
+	/// <summary>
+	/// Checks if the result is faulted and satisfies the given condition.
+	/// </summary>
+	/// <param name="self">The Result object.</param>
+	/// <param name="function">The function to check the error condition.</param>
+	/// <returns>True if the Result object represents a failed operation and meets the specified condition; otherwise, false.</returns>
+	public static bool IsFailedAnd(this Result self, Func<Error, bool> function) =>
+		self.IsFaulted && function(self.Error()!);
+
+	/// <summary>
+	/// Checks if the result is faulted and satisfies the given condition.
+	/// </summary>
+	/// <typeparam name="TValue">The type of the value contained in case of successful result.</typeparam>
+	/// <param name="self">The result to be checked.</param>
+	/// <param name="function">The condition to be evaluated.</param>
+	/// <returns>
+	/// <c>true</c> if the result is failed and satisfies the specified condition; otherwise, <c>false</c>.
+	/// </returns>
+	public static bool IsFailedAnd<TValue>(this Result<TValue> self, Func<Error, bool> function) =>
+		self.IsFaulted && function(self.Error()!);
+}
