c4f7180ea057fe0802c250b7c913af21acc2ea2c
Carlos J. Ortiz <ortizd@gmail.com>
2024-05-07 11:56:44 -0400

    update FlattenResultExtensions.cs

--- src/Results/Extensions/FlattenResultExtensions.cs
+++ src/Results/Extensions/FlattenResultExtensions.cs
@@ -8,20 +8,6 @@ namespace Results.Extensions;
 public static class FlattenResultExtensions
 {
 	/// <summary>
-	/// Flattens a nested result by returning the inner result if it is successful,
-	/// otherwise constructs a new error result using the errors from the outer result.
-	/// </summary>
-	/// <param name="self">The nested result to flatten.</param>
-	/// <returns>
-	/// The inner result if it is successful,
-	/// otherwise a new error result with the errors from the outer result.
-	/// </returns>
-	public static Result Flatten(this Result<Result> self)
-		=> self.IsSucceed
-			? self.Value
-			: new Error(self.Errors);
-
-	/// <summary>
 	/// Flattens a nested result by extracting the inner result if the outer result is successful,
 	/// or creates a new error result containing the errors from the outer result.
 	/// </summary>
