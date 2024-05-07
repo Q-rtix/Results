e6c365d67b58521aed6644c5a7e54ae579f348b1
Carlos J. Ortiz <ortizd@gmail.com>
2024-05-07 11:51:54 -0400

    update CheckResultExtensions.cs

--- src/Results/Extensions/CheckResultExtensions.cs
+++ src/Results/Extensions/CheckResultExtensions.cs
@@ -12,17 +12,18 @@ public static class CheckResultExtensions
 	/// </summary>
 	/// <typeparam name="TValue">The type of the value contained in case of successful result.</typeparam>
 	/// <param name="self">The result object to check.</param>
-	/// <param name="function">The condition function to apply on the result value.</param>
+	/// <param name="predicate">The condition function to apply on the result value.</param>
 	/// <returns>True if the result is successful and satisfies the condition, otherwise false.</returns>
-	public static bool IsSucceedAnd<TValue>(this Result<TValue> self, Func<TValue, bool> function)
-		=> self.IsSucceed && function(self.Value);
-
+	public static bool IsSucceedAnd<TValue>(this Result<TValue> self, Func<TValue, bool> predicate)
+		=> self.IsSucceed && predicate(self.Value);
+	
 	/// <summary>
-	/// Checks if the result is faulted and satisfies the given condition.
+	/// Checks if the result is failed and satisfies the given condition.
 	/// </summary>
-	/// <param name="self">The Result object.</param>
-	/// <param name="function">The function to check the error condition.</param>
-	/// <returns>True if the Result object represents a failed operation and meets the specified condition; otherwise, false.</returns>
-	public static bool IsFailedAnd(this Result self, Func<Error, bool> function) =>
-		self.IsFaulted && function(self.Error()!);
+	/// <typeparam name="TValue">The type of the value contained in case of successful result.</typeparam>
+	/// <param name="self">The result object to check.</param>
+	/// <param name="predicate">The condition function to apply on the result value.</param>
+	/// <returns>True if the result is failed and satisfies the condition, otherwise false.</returns>
+	public static bool IsFaultedAnd<TValue>(this Result<TValue> self, Func<Error, bool> predicate)
+		=> self.IsFaulted && predicate(self.Error()!);
 }
